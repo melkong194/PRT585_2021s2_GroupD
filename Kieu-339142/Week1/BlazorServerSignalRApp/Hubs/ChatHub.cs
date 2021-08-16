006a8ecb59ecb4ddbc4cfa4f1e4a069fd5456fc8
Melko <melkong194@gmail.com>
2021-08-16 13:01:53 +0930

    BlazorServerSignalR lesson

---
+++ Kieu-339142/Week1/BlazorServerSignalRApp/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorServerSignalRApp.Server.Hubs;
+
+namespace BlazorServerSignalRApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
