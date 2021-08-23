287f75b921881a3e8793c7f3967aa5403ea3ba06
DrakeLiam <plamvip1999@gmail.com>
2021-08-24 04:29:16 +0930

    PhongLamNguyen-s331150: MVC tutorial on MS Docs

---
+++ PhongLamNguyen-s331150/Week2/TaskA/TaskA/Data/TaskAContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskA.Models;
+
+namespace TaskA.Data
+{
+    public class TaskAContext : DbContext
+    {
+        public TaskAContext (DbContextOptions<TaskAContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskA.Models.Movie> Movie { get; set; }
+    }
+}
