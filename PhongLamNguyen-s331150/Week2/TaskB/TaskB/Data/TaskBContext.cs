718ea0026e60e709f3d9b15639ed2e8f34237627
DrakeLiam <plamvip1999@gmail.com>
2021-08-24 08:19:54 +0930

    PhongLamNguyen-s331150: Task B - Finish all basic requirements (except database connection and customization in home index)

---
+++ PhongLamNguyen-s331150/Week2/TaskB/TaskB/Data/TaskBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskB.Models;
+
+namespace TaskB.Data
+{
+    public class TaskBContext : DbContext
+    {
+        public TaskBContext (DbContextOptions<TaskBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskB.Models.Movie> Movie { get; set; }
+
+        public DbSet<TaskB.Models.Category> Category { get; set; }
+    }
+}
