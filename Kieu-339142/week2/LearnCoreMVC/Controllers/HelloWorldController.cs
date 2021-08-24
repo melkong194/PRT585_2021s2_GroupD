8b65c6267bdadec19d1539a8ade5997508bd933a
Melko <melkong194@gmail.com>
2021-08-24 22:09:35 +0930

    Adding View Done

--- Kieu-339142/week2/LearnCoreMVC/Controllers/HelloWorldController.cs
+++ Kieu-339142/week2/LearnCoreMVC/Controllers/HelloWorldController.cs
@@ -9,17 +9,20 @@ namespace LearnCoreMVC.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
 
     }
