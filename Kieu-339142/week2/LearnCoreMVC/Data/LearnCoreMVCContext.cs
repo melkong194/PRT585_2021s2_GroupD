e01bd0f293da5b62397b6d1fd42c118e25175ed6
Melko <melkong194@gmail.com>
2021-08-24 23:32:58 +0930

    Adding a model done

---
+++ Kieu-339142/week2/LearnCoreMVC/Data/LearnCoreMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LearnCoreMVC.Models;
+
+namespace LearnCoreMVC.Data
+{
+    public class LearnCoreMVCContext : DbContext
+    {
+        public LearnCoreMVCContext (DbContextOptions<LearnCoreMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LearnCoreMVC.Models.Movie> Movie { get; set; }
+    }
+}
