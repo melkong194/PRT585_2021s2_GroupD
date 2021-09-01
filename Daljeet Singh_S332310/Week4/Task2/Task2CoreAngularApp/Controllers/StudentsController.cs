f0b1e9d4691b23fb6bde303be707ef8f44f0dc36
daljeet0611 <70369517+daljeet0611@users.noreply.github.com>
2021-09-01 16:22:30 +0500

    Add files via upload

---
+++ Daljeet Singh_S332310/Week4/Task2/Task2CoreAngularApp/Controllers/StudentsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Task2CoreAngularApp.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Task2CoreAngularApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: api/<StudentsController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+          
+        {
+            List<Student> oStudents = new List<Student>() { 
+            new Student(){ Id=1,Name="Peter",Roll= 1022},
+            new Student(){ Id=2,Name="Parker",Roll= 1023},
+            new Student(){ Id=3,Name="Zan",Roll= 1024},
+            };
+            return oStudents;
+        }
+
+        // GET api/<StudentsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StudentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Daljeet Singh_S332310/Week4/Task2/Task2CoreAngularApp/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task2CoreAngularApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public int Roll { get; set; } = 0;
+
+    }
+}
