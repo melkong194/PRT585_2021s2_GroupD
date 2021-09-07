c2c179f32671b0589ff2465f88237b0783fe087a
daljeet0611 <70369517+daljeet0611@users.noreply.github.com>
2021-09-07 23:34:56 +0500

    Add files via upload

---
+++ Daljeet Singh_S332310/Week5/AngularCoreTaskWeek5/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularCoreWeek5Task.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        public enum LanguageEnum
+        {
+            English,
+            Japanese,
+            Chinese
+        }
+        public LanguageEnum Language { get; set; }
+        public Category Category { get; set; }
+    }
+}
