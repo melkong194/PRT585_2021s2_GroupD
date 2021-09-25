da4a83d24d7870f9a0b88f7e24efff61ac185bb9
DrakeLiam <plamvip1999@gmail.com>
2021-09-26 04:34:29 +0930

    PhongLamNguyen-s331150: Client App main view

--- PhongLamNguyen-s331150/SmartConstructing/SmartConstructing/ClientApp/src/app/home/home.component.ts
+++ PhongLamNguyen-s331150/SmartConstructing/SmartConstructing/ClientApp/src/app/home/home.component.ts
@@ -1,24 +1,29 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
   link: string = "";
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   public checkLoginType() {
     var toggle = <HTMLInputElement>document.getElementById("switch");
     var inputUserName = <HTMLInputElement>document.getElementById("inputUserName");
 
     if (inputUserName.value == "") {
-      inputUserName.classList.add("is-invalid");
+      inputUserName.classList.add("is-invalid"); //remove after jquery
     } else {
       if (!toggle.checked) {
-        this.link = "index";
+        this.router.navigate(['/index']);
       } else if (toggle.checked) {
-        this.link = "mbindex";
+        this.router.navigate(['/mbindex']);
       }
     }
 
--- PhongLamNguyen-s331150/SmartConstructing/SmartConstructing/ClientApp/src/app/mbindex/mbindex.component.ts
+++ PhongLamNguyen-s331150/SmartConstructing/SmartConstructing/ClientApp/src/app/mbindex/mbindex.component.ts
@@ -6,4 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./mbindex.component.css']
 })
 export class MbIndexComponent {
+  myDate: string;
+  myTime: string;
+
+  locations = [
+    { lat: -12.371611433792701, lng: 130.8687731560323 },
+  ];
+
+  icon = {
+    url: 'https://toppng.com/uploads/preview/app-icon-set-login-icon-comments-avatar-icon-11553436380yill0nchdm.png',
+    scaledSize: {
+      width: 30,
+      height: 30
+    }
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      let date = new Date();
+      this.myTime = date.toTimeString().substring(0, 8);
+      this.myDate = date.toDateString();
+    }, 1000);
+  }
+
 }
