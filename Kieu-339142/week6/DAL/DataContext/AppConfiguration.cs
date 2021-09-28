e5bca0a749b2604f66117a8c92311fce9f064471
Melko <melkong194@gmail.com>
2021-09-28 16:36:59 +0930

    week 6: initial

--- Kieu-339142/week6/DAL/DataContext/AppConfiguration.cs
+++ Kieu-339142/week6/DAL/DataContext/AppConfiguration.cs
@@ -1,26 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-
 
 namespace DAL.DataContext
 {
     public class AppConfiguration
     {
+        //CONSTRUCTOR
         public AppConfiguration()
         {
+            //Configuration Builder - Used to obtain configuration settings from a config/settings file (Builds a key/value structure).
             ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            //Setting the path to our appsettings.json file.
             string path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            //Pass/Add the json files (Settings file) content to the configuration builder.
             configurationBuilder.AddJsonFile(path, false);
+            //The builds a object consisting of the contents of settings file and maps them to a key/value structure/object [root is our object]
             IConfigurationRoot root = configurationBuilder.Build();
+            //Obtains the value we are requiring by providing the root settings object the key for the value we want. 
             IConfigurationSection appSettings = root.GetSection("ConnectionStrings:DefaultConnection");
+            //Simply assigning the key value (connection string) to the SqlConnectionVariable so it can be accessed when we init this class
             SqlConnectionString = appSettings.Value;
         }
 
-        public string SqlConnectionString { get; set; }
+        public String SqlConnectionString { get; set; }
     }
 }
