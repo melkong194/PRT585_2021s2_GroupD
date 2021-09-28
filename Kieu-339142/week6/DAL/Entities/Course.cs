151626b1df9b7d00f233cf16c21bd1ceb2293f76
Melko <melkong194@gmail.com>
2021-09-28 16:42:15 +0930

    Week6: Add 3 entities

---
+++ Kieu-339142/week6/DAL/Entities/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Course
+    {
+        public Int64 CourseID { get; set; }
+        public String Course_Name { get; set; }
+        public String Course_Code { get; set; }
+    }
+}
