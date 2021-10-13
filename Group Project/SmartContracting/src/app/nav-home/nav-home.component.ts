0159ebcc0e377c317777f7935552ed8f9ff6cfb9
DrakeLiam <plamvip1999@gmail.com>
2021-10-13 10:04:49 +0930

    Add exceptionless handler

--- Group Project/SmartContracting/src/app/nav-home/nav-home.component.ts
+++ Group Project/SmartContracting/src/app/nav-home/nav-home.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 
+import { ExceptionlessClient } from 'exceptionless';
+const defaultClient = ExceptionlessClient.default;
+const config = {
+  apiKey: "yDSlIwzKI5AUV2czExRcV71grIuED7dIyVLxshhH", 
+  serverUrl: "http://localhost:7000",
+};
+const client = new ExceptionlessClient(config);
+
 @Component({
   selector: 'app-nav-home',
   templateUrl: './nav-home.component.html',
@@ -50,6 +58,10 @@ export class NavHomeComponent implements OnInit {
     };
 
     this.router.navigate(['/timesheet'], navigationExtras);
+  }
+
+  exceptionless() {
+    client.submitException(new Error("Testing!!"));
   }
 
 }
